b608a55847fcbf02c0c4b22ddac6b7ea1286fc0d
Travis <arma3cloud@gmail.com>
2022-03-02 19:25:21 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="A challenge website from https://www.frontendmentor.io" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <link rel="stylesheet" href="styles.css">
+        <title>Profile Card</title>
+    </head>
+    <body>
+		
+    </body>
+</html>
